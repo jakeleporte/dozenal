6388019c31a0cff7862cb74aeaf6f532298ec287
Donald P. Goodman III <dgoodmaniii@gmail.com>
2010-02-15 13:23:49 -0500

    Fixed some rounding issues in dozenal conversion; v0.99.4.

--- dec/testing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#+AMDG
-
-./dec -e "E;3";
-./dec "E;3";
-./dec -e < testfile;
-./dec < testfile;
--- doz/double.c
+++
@@ -1,9 +0,0 @@
-/* +AMDG */
-
-#include<stdio.h>
-#include<float.h>
-
-int main(void)
-{
-	printf("%e\n",DBL_MIN);
-}
