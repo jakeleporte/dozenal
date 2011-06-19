2913f21545f78f17553b742584e1dc597814a798
Donald P. Goodman III <dgoodmaniii@gmail.com>
2011-06-19 01:03:45 -0400

    Tracking some additional files.
    Somehow Makefiles dropped out for doz, dozdc, and tgmconv.
    Also, added the newly necessary dec.c to the doz tree and a
    small new testing file for doz.

---
+++ dozenal/doz/testing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# +AMDG
+
+echo "One third:  `./doz 0.3333333333333333333333333`";
+echo "One third:  `./doz 0.3333333333333333`";
+echo "Two thirds:  `./doz 0.66666666666666666666666`";
+echo "Two thirds:  `./doz 0.666666666666666`";
+echo "Smallest:  `./doz -ek19 4.9406564584124654e-324`";
