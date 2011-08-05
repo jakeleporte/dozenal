afb204816fa351f5fc8ef8d4394ae5c775276a77
Donald P. Goodman III <dgoodmaniii@gmail.com>
2011-08-05 00:50:00 -0400

    Fixed angstroms; v2.7.

--- package.sh
+++ package.sh
@@ -5,10 +5,10 @@
 # the Immaculate Heart of Mary for her prayers, and to the
 # Sacred Heart of Jesus for His mercy.
 
-tar -cvvf dozenal_v2.5_src.tar ./dozenal/dec ./dozenal/doz \
+tar -cvvf dozenal_v2.7_src.tar ./dozenal/dec ./dozenal/doz \
 ./dozenal/dozdc ./dozenal/tgmconv ./dozenal/dozword \
 ./dozenal/install.sh ./dozenal/uninstall.sh;
-tar -f dozenal_v2.5_src.tar --wildcards --verbose --delete *.o ./dozenal/dozdc/dozdc ./dozenal/doz/doz ./dozenal/dec/dec ./dozenal/tgmconv/tgmconv ./dozenal/dozword/dozword;
-gzip dozenal_v2.5_src.tar;
-tar -cvvf dozenal_v2.5_bin_x86_64.tar ./dozenal/doz/doz ./dozenal/dec/dec ./dozenal/dozdc/dozdc ./dozenal/dozword/dozword ./dozenal/tgmconv/tgmconv;
-gzip dozenal_v2.5_bin_x86_64.tar;
+tar -f dozenal_v2.7_src.tar --wildcards --verbose --delete *.o ./dozenal/dozdc/dozdc ./dozenal/doz/doz ./dozenal/dec/dec ./dozenal/tgmconv/tgmconv ./dozenal/dozword/dozword;
+gzip dozenal_v2.7_src.tar;
+tar -cvvf dozenal_v2.7_bin_x86.tar ./dozenal/doz/doz ./dozenal/dec/dec ./dozenal/dozdc/dozdc ./dozenal/dozword/dozword ./dozenal/tgmconv/tgmconv;
+gzip dozenal_v2.7_bin_x86.tar;
