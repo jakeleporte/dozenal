febc3945ecceb4e333cdaac5bed563cb2c31ac59
Donald P. Goodman III <dgoodmaniii@gmail.com>
2012-08-24 20:46:05 -0400

    Fixing the shell script testing.

--- dozenal/dozdate/test_symm.sh
+++ dozenal/dozdate/test_symm.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 ./symm_run.sh > symm_results.txt;
-diff symm_ans.txt symm_results.txt;
+diff symm_ans.txt symm_results.txt > /dev/null;
+if [ $? -eq 0 ]; then
+	echo "Gregorian to Symm676 works!"
+else
+	echo "Gregorian to Symm676 doesn't work!"
+	diff symm_ans.txt symm_results.txt;
+fi
