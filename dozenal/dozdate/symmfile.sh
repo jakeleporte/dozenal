2cd0f244eb638031f977b3986d1cfdd921ee8b58
Donald P. Goodman III <dgoodmaniii@gmail.com>
2012-08-30 13:59:28 -0400

    Some testing tweaks.

---
+++ dozenal/dozdate/symmfile.sh
@@ -0,0 +1,7 @@
+# +AMDG  This document was begun on 26 Aug 11X8, the feast of
+# St. Rose of Lima, and it is humbly dedicated to her and to
+# the Immaculate Heart of Mary for their prayers, and to the
+# Sacred Heart of Jesus for His mercy.
+
+./dozdate -Ssf symmfile.txt > symm_results_2.txt;
+diff symm_results_2.txt symmans.txt;
