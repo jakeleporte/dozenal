e02f5c775c765bb01403eaac65acfde5ce6cb626
Donald P. Goodman III <dgoodmaniii@gmail.com>
2013-01-05 20:58:04 -0500

    latexcal:  continuing to format the monthly display cal.

--- package.sh
+++ package.sh
@@ -5,11 +5,11 @@
 # the Immaculate Heart of Mary for her prayers, and to the
 # Sacred Heart of Jesus for His mercy.
 
-tar -cvvf dozenal_v11E90101_src.tar ./dozenal/dec ./dozenal/doz \
+tar -cvvf dozenal_v11E90105_src.tar ./dozenal/dec ./dozenal/doz \
 ./dozenal/dozdc ./dozenal/tgmconv ./dozenal/dozword \
 ./dozenal/dozpret ./dozenal/dozdate \
 ./dozenal/install.sh ./dozenal/uninstall.sh;
-tar -f dozenal_v11E90101_src.tar --wildcards --verbose --delete *.o ./dozenal/dozdc/dozdc ./dozenal/doz/doz ./dozenal/dec/dec ./dozenal/tgmconv/tgmconv ./dozenal/dozword/dozword ./dozenal/dozpret/dozpret ./dozenal/dozdate/dozdate;
-gzip dozenal_v11E90101_src.tar;
-tar -cvvf dozenal_v11E90101_bin_x86.tar ./dozenal/doz/doz ./dozenal/dec/dec ./dozenal/dozdc/dozdc ./dozenal/dozword/dozword ./dozenal/tgmconv/tgmconv ./dozenal/dozpret/dozpret ./dozenal/dozdate/dozdate ./dozenal/dozdate/gpl-3.0.txt;
-gzip dozenal_v11E90101_bin_x86.tar;
+tar -f dozenal_v11E90105_src.tar --wildcards --verbose --delete *.o ./dozenal/dozdc/dozdc ./dozenal/doz/doz ./dozenal/dec/dec ./dozenal/tgmconv/tgmconv ./dozenal/dozword/dozword ./dozenal/dozpret/dozpret ./dozenal/dozdate/dozdate;
+gzip dozenal_v11E90105_src.tar;
+tar -cvvf dozenal_v11E90105_bin_x86.tar ./dozenal/doz/doz ./dozenal/dec/dec ./dozenal/dozdc/dozdc ./dozenal/dozword/dozword ./dozenal/tgmconv/tgmconv ./dozenal/dozpret/dozpret ./dozenal/dozdate/dozdate ./dozenal/dozdate/gpl-3.0.txt;
+gzip dozenal_v11E90105_bin_x86.tar;
