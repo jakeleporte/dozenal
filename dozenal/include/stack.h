a65b50a17e24a2b8135711b2f8930d891fe34232
Donald Patrick Goodman III <dgoodmaniii@gmail.com>
2021-05-12 15:30:40 -0400

    Stack doesn't work in dozdc with tcc compilation.

--- dozenal/include/stack.h
+++ dozenal/include/stack.h
@@ -1,34 +1,38 @@
-/* +AMDG */
+/* + AMDG */
 /*
- * This document was begun on 15 January 2010, the feast of
- * St. Paul the Hermit, and it is humbly dedicated to him
- * and to the Immaculate Heart of Mary for their prayers,
- * and to the Sacred Heart of Jesus for His mercy.
- */
-/*
- * The library containing the stack functions for dozdc.
+ * This document was begun on 16 Aug 1201, the feast of St.
+ * Agapitus, M, and it is humbly dedicated to him and to the
+ * Immaculate Heart of Mary for their prayers, and to the
+ * Sacred Heart of Jesus for His mercy.
  *
- * (C) Copyright 2010, 2011  Donald P. Goodman III
+ * dozcal:  keeps a calendar with a specific internal format
+ * in dozenal
  *
- * This file is part of dozdc.
+ * (C) Copyright 2016  Donald P. Goodman III
  *
- * dozdc is free software:  you can redistribute it and/or
+ * This file is part of dozcal.
+ *
+ * dozcal is free software:  you can redistribute it and/or
  * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any
  * later version.
  *
- * dozdc is distributed in the hope that it will be
+ * dozcal is distributed in the hope that it will be
  * useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE.  See the GNU General Public License
  * for more details.
  *
  * You should have received a copy of the GNU General Public
- * License along with dozdc.  If not, see
+ * License along with dozcal.  If not, see
  * <http://www.gnu.org/licenses/>.
  */
 
-void push(double f);
-double pop(void);
 int clean(void);
+int swap_top(void);
+int dupe_top(void);
+int print_stack(void);
+int clear_stack(void);
+double pop(void);
+void push(double f);
